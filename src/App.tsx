ce5f1ce8ae06042545571f5d92dff0b484868260
Aterfax <Aterfax@users.noreply.github.com>
2024-12-08 15:30:48 +0000

    Push only Afghanistan

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { Falklands } from "./dcs/maps/Falklands";
 import { Normandy } from "./dcs/maps/Normandy";
 import { TheChannel } from "./dcs/maps/TheChannel";
 import { Nevada } from "./dcs/maps/Nevada";
+import { Afghanistan } from "./dcs/maps/Afghanistan";
 import { Server, serverStore } from "./stores/ServerStore";
 import { route } from "./util";
 
@@ -147,6 +148,8 @@ function ServerContainer({ serverName }: { serverName: string }) {
     dcsMap = Normandy;
   } else if (refLat >= 32 && refLat <= 40 && refLng >= -122 && refLng <= -112) {
     dcsMap = Nevada;
+  } else if (refLat >= 28 && refLat <= 39 && refLng >= 60 && refLng <= 74) {
+	dcsMap = Afghanistan;
   } else {
     console.log(refLat, refLng);
     return (
